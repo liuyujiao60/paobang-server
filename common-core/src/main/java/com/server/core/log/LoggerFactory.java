e653e9a71e31930bf1cad6da71ff47b8dcec84db
liuyujiao <liuyujiao@mail.hdzuoye.com>
2018-07-24 15:37:49 +0800

    update

--- common-core/src/main/java/com/server/core/log/LoggerFactory.java
+++ common-core/src/main/java/com/server/core/log/LoggerFactory.java
@@ -4,13 +4,7 @@ import org.slf4j.Logger;
 
 public class LoggerFactory {
 	
-	private static final String QUESTION_SPIDER = "questionSpider";
-	
 	private static final String REQUEST_INFO = "requestInfo";
-	
-	public static Logger getQuestionSpider(Class<?> clazz) {
-        return org.slf4j.LoggerFactory.getLogger(QUESTION_SPIDER);
-    }
 	
 	public static Logger getRequestInfoLogger(Class<?> clazz) {
         return org.slf4j.LoggerFactory.getLogger(REQUEST_INFO);
